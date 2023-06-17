7acea6c77a7f2b16d4f5b086b5db749dd40be068
rmdaniii <ahmedtramadan4@gmail.com>
2023-06-17 15:10:03 +0200

    Biometric Authentication

--- app/src/main/java/com/example/gamingstoreui/MainActivity.kt
+++ app/src/main/java/com/example/gamingstoreui/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.gamingstoreui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import com.example.gamingstoreui.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,11 +19,73 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnContinue.setOnClickListener {
-
-            val intent:Intent = Intent(this@MainActivity, HomeActivity::class.java)
-            startActivity(intent)
+            if (isBiometricSupported()) {
+                showBiometricPrompt()
+            } else {
+                Toast.makeText(this, "Something Error", Toast.LENGTH_SHORT).show()
+            }
 
         }
-
     }
+
+    private fun showBiometricPrompt() {
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric Authentication")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("Cancel")
+            .build()
+
+        val biometricPrompt = BiometricPrompt(this, ContextCompat.getMainExecutor(this),
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    // Handle authentication error
+                    showMessage("Authentication error: $errString")
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    // Handle authentication success
+                    showMessage("Authentication succeeded!")
+                    val intent: Intent = Intent(this@MainActivity, HomeActivity::class.java)
+                    startActivity(intent)
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    // Handle authentication failure
+                    showMessage("Authentication failed.")
+                }
+            })
+
+        biometricPrompt.authenticate(promptInfo)
+    }
+
+
+    private fun showMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun isBiometricSupported(): Boolean {
+        val biometricManager = BiometricManager.from(this)
+        val canAuthenticate =
+            biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_WEAK)
+        when (canAuthenticate) {
+            BiometricManager.BIOMETRIC_SUCCESS -> {
+                // The user can authenticate with biometrics, continue with the authentication process
+                return true
+            }
+
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE, BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE, BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                // Handle the error cases as needed in your app
+                return false
+            }
+
+            else -> {
+                // Biometric status unknown or another error occurred
+                return false
+            }
+        }
+    }
+
 }
